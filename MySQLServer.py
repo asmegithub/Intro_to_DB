004fda05a8b4b8e2f8fdbf1d44c080e21c5724b3
Asmare Zelalem <asmarestu12@gmail.com>
2024-07-29 13:25:41 +0300

    chore: Update MySQLServer.py to create alx_book_store database

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,14 +1,21 @@
 import mysql.connector
 
+try:
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="new_password"
+    )
+    cursor = connection.cursor()
 
-connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="new_password",
-)
-cursor = connection.cursor()
-cursor.execute("CREATE DATABASE alx_book_store")
-print("Database 'alx_book_store' created successfully!")
+    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 
-connection.close()
-cursor.close()
+    print("Database 'alx_book_store' created successfully!")
+
+except mysql.connector.Error as error:
+    print("Failed to connect to MySQL server:", error)
+
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
