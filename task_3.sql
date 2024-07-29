e52f7f16320f2f7b77ef9d89b7c2477512146c60
Asmare Zelalem <asmarestu12@gmail.com>
2024-07-29 13:53:09 +0300

    chore: Refactor task_3.sql to use SHOW TABLES command

--- task_3.sql
+++ task_3.sql
@@ -1,13 +1,3 @@
-#!/bin/bash
+USE alx_book_store;
 
--- # Check if the database name is provided as an argument
-if [ $# -eq 0 ]; then
-    echo "Please provide the database name as an argument."
-    exit 1
-fi
-
--- # Store the database name in a variable
-database=$1
-
--- # Execute the MySQL command to list all tables in the specified database
-mysql -e "USE $database; SHOW TABLES;"
+SHOW TABLES;
